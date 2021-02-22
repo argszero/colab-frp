d59e34d365a8a6e035b5177aedbbdc4e84997dc5
argszero <argszero@gmail.com>
2021-02-22 15:19:55 +0800

    init

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="colab_frp",
-    version="0.0.3",
+    version="0.0.4",
     author="argszero",
     author_email="argszero@gmail.com",
     description="Google colab frp connector",
